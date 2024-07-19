aa35de617c6fd11d1632fa31d1fe7c458c734877
slashTeen <1900271083@qq.com>
2024-07-19 10:50:45 +0800

    changed

--- test/test2.cpp
+++ test/test2.cpp
@@ -3,13 +3,7 @@
 using namespace std;
 using ll = long long;
 
-void solve() {
-    vector<int> vec {1, 3, 5, 6};
-    vec.resize(10);
-    for (int& v : vec) {
-        cout << v << " ";
-    }
-}
+void solve() {}
 
 int main() {
     ios::sync_with_stdio(false);
