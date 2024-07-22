8b75d8596b94c2e955900067ac9a9008e4f92a80
slashTeen <1900271083@qq.com>
2024-07-22 13:09:07 +0800

    upd

--- Problems/Leetcode/3001-4000/3201-3300/3224.cpp
+++ Problems/Leetcode/3001-4000/3201-3300/3224.cpp
@@ -2,11 +2,39 @@
 
 using namespace std;
 
+/*
+    枚举
+*/
+
 class Solution {
 public:
     int minChanges(vector<int>& nums, int k) {
+        vector<int> cnt(k + 1), add(k + 1);
         int n = nums.size();
-        unordered_map<int, int> map;
+        for (int i = 0; i < n / 2; ++i) {
+            int p = nums[i], q = nums[n - 1 - i];
+            if (p > q) {
+                swap(p, q);
+            }
+            cnt[q - p]++;
+            add[max(q, k - p)]++;
+        }
+
+        int ans = INT_MAX;
+        int sum = 0;
+        for (int i = 0; i <= k; ++i) {
+            ans = min(ans, n / 2 - cnt[i] + sum);
+            sum += add[i];
+        }
+        return ans;
+    }
+};
+
+class my_Solution {
+public:
+    int minChanges(vector<int>& nums, int k) {
+        int n = nums.size();
+        map<int, int> map;
         vector<int> vec(n / 2);
         for (int i = 0; i < n / 2; ++i) {
             if (nums[i] > nums[n - 1 - i]) {
--- Problems/Leetcode/contest/Weekly/test.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-
-
-class Solution:
-    def minimumOperations(self, nums: List[int], target: List[int]) -> int:
-        lst = [map(lambda x, y: x - y, target, nums)]
-        print(lst)
-        return 0
