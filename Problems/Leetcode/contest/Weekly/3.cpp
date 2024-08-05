aa497589be637ff1972a7298554e83683b7fc915
slashTeen <1900271083@qq.com>
2024-08-05 18:48:29 +0800

    upd

--- Problems/Leetcode/contest/Weekly/3.cpp
+++ Problems/Leetcode/contest/Weekly/3.cpp
@@ -6,7 +6,32 @@ static constexpr int MOD = 1e9 + 7;
 
 class Solution {
 public:
+    // 用并查集实现边的合并（并查集的节点表示 i->i+1 这条边）
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
+        vector<int> pa(n - 1);
+        iota(pa.begin(), pa.end(), 0);
+
+        function<int(int)> find = [&](int x) {
+            return x == pa[x] ? x : pa[x] = find(pa[x]);
+        };
+
+        vector<int> ans;
+        int cnt = n - 1;
+        for (auto& q : queries) {
+            int l = q[0], r = q[1] - 1; // 把 l, l+1, l+2, ... , r-2 都连到 r-1 上
+            int root = find(r);
+            for (int i = find(l); i < r; i = find(i + 1)) {
+                pa[i] = root;
+                cnt--;
+            }
+            ans.emplace_back(cnt);
+        }
+        return ans;
+    }
+
+public:
+    // set 去边
+    vector<int> shortestDistanceAfterQueries_1(int n, vector<vector<int>>& queries) {
         set<pair<int, int>> s;
         for (int i = 0; i < n - 1; ++i) {
             s.emplace(i, i + 1);
