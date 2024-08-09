e5b1cb7256983380eeee115d4247b9695588a304
slashTeen <1900271083@qq.com>
2024-08-09 16:45:16 +0800

    upd

---
+++ test/contest.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+static constexpr int MOD = 1e9 + 7;
+
+void solve() {
+    vector<int> vec {4, 2, 6, 2, 1};
+    int n = vec.size();
+    vector<int> pre(n + 1);
+    partial_sum(vec.begin(), vec.end(), pre.begin() + 1);
+
+    for (int& v : pre) {
+        cout << v << " ";
+    }
+
+    system("echo Hello > out.txt");
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+
+    return 0;
+}
---
+++ test/contest.py
@@ -0,0 +1,8 @@
+from collections import defaultdict, deque
+from typing import List
+from sortedcontainers import sortedlist
+
+fmax = lambda x, y: x if x > y else y
+fmin = lambda x, y: x if x < y else y
+MOD = 1_000_000_007
+
