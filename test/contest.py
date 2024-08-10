3729e7fc31828746fe91daa17b0f3d3c7c29fb54
slashTeen <1900271083@qq.com>
2024-08-10 10:46:36 +0800

    add some code

--- test/contest.py
+++ test/contest.py
@@ -1,8 +1,8 @@
 from collections import defaultdict, deque
+from functools import cache
 from typing import List
-from sortedcontainers import sortedlist
+from sortedcontainers import SortedList
 
 fmax = lambda x, y: x if x > y else y
 fmin = lambda x, y: x if x < y else y
 MOD = 1_000_000_007
-
