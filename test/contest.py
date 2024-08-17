8ec051844d3c17ae2a26ab4fd958ac377abb0ea0
slashTeen <1900271083@qq.com>
2024-08-17 11:05:22 +0800

    update

--- test/contest.py
+++
@@ -1,9 +0,0 @@
-from functools import cache
-from typing import List
-
-# collections  sortedcontainers  more_itertools
-
-fmax = lambda x, y: x if x > y else y
-fmin = lambda x, y: x if x < y else y
-
-MOD = 1_000_000_007
