398f2d680b581dc560a9656cebffd56e9fd6800e
slashTeen <1900271083@qq.com>
2024-08-25 21:51:37 +0800

    new file:   Problems/Atcoder/ABC343/E_7_x_7_x_7.cpp 	modified:   Problems/Atcoder/ABC344/C_A_B_C.cpp 	new file:   Problems/Atcoder/ABC344/D_String_Bags.cpp 	new file:   Problems/Atcoder/ABC344/E_Insert_or_Erase.cpp 	new file:   "Problems/Atcoder/ABC344/\357\274\210todo\357\274\211E_Insert_or_Erase.cpp" 	new file:   "Problems/nowcoder/Round 57/A.\345\260\217\347\272\242\345\226\234\346\254\242_1.cpp" 	new file:   "Problems/nowcoder/Round 57/B.\345\260\217\347\272\242\347\232\204\346\240\221\345\210\207\345\211\262.cpp" 	new file:   "Problems/nowcoder/Round 57/C.\345\260\217\347\272\242\347\232\204\345\217\214\345\245\275\346\225\260_easy.cpp" 	new file:   "Problems/nowcoder/Round 57/F.\345\260\217\347\272\242\347\232\204\346\225\260\347\273\204\346\223\215\344\275\234.cpp"

--- Problems/Atcoder/ABC344/C_A_B_C.cpp
+++ Problems/Atcoder/ABC344/C_A_B_C.cpp
@@ -9,29 +9,29 @@ void solve() {
     int n;
     cin >> n;
     vector<int> v1(n);
-    for (int& v : v1) {
-        cin >> v;
+    for (int i = 0; i < n; ++i) {
+        cin >> v1[i];
     }
 
     int m;
     cin >> m;
     vector<int> v2(m);
-    for (int& v : v2) {
-        cin >> v;
+    for (int i = 0; i < m; ++i) {
+        cin >> v2[i];
     }
-    
+
     int l;
     cin >> l;
     vector<int> v3(l);
-    for (int& v : v3) {
-        cin >> v;
+    for (int i = 0; i < l; ++i) {
+        cin >> v3[i];
     }
 
-    unordered_map<int, int> ump;
+    set<int> s;
     for (int& n1 : v1) {
         for (int& n2 : v2) {
             for (int& n3 : v3) {
-                ump[n1 + n2 + n3]++;
+                s.emplace(n1 + n2 + n3);
             }
         }
     }
@@ -41,14 +41,13 @@ void solve() {
     while (q--) {
         int num;
         cin >> num;
-        cout << (ump[num] > 0 ? "Yes" : "No") << "\n";
+        cout << (s.contains(num) ? "Yes" : "No") << "\n";
     }
 }
 
-signed main() {
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
 
     solve();
 
